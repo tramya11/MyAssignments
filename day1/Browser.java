1f424631c0d464c42399c4468b30d626e8705ee4
tramya11 <tramya11@gmail.com>
2023-10-15 13:24:26 +0530

    Add files via upload
    Day1

---
+++ day1/Browser.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		float browserVersion = 117.1f;
+		String browserName = "Chrome";
+		boolean pageLoaded = true;
+		int noOfBrowser = 3;
+		char browserLogo = 'C';
+		System.out.println(browserVersion);
+		System.out.println(browserName);
+		System.out.println(pageLoaded);
+		System.out.println(noOfBrowser);
+		System.out.println(browserLogo);
+		
+	}
+}
---
+++ day1/FindNumber.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		
+		int number = 45;
+		if (number>0) {
+			System.out.println("The Number is positive");
+		}
+		else {
+			System.out.println("The Number is negative");
+		}
+				
+	}
+}
